8fa08aae080027d315457b718a415fdaba78de47
Gbardis <crisolivasavatage@hotmail.com>
2018-03-13 14:32:52 +0200

    create regression template

--- regression_template.py
+++ regression_template.py
@@ -13,40 +13,28 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Fitting Linear Regression to the dataset
-from sklearn.linear_model import LinearRegression
+# Fitting the Regression to the dataset
 
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+# Create a new Regressor
 
-# Fitting Polynomial Regression to the dataset
+# Predicting  a new result
+y_pred = regressor.predict(6.5)
 
-from sklearn.preprocessing import PolynomialFeatures
-
-poly_reg = PolynomialFeatures(degree=4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
-
-# Visualising the linear Regression results
+# Visualising the Polynomial Regression results
 plt.scatter(X, y, color='red')
-plt.plot(X, lin_reg.predict(X), color='blue')
-plt.title("Truth or bluff (Linear Regression)")
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title("Truth or bluff (Regression Model)")
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-# Visualising the Polynomial Regression results
+
+# Visualising the Regressor results (for higher resolution and smoother curve)
+
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
-plt.title("Truth or bluff (Polynomial Regression)")
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title("Truth or bluff (Regression Model)")
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-
-# Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
-
-# Predicting  anew result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
